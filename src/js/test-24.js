879c2dab27cf8b4dd98d6cfc7ae938ef99b3ba1c
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-18 18:45:12 +0200

    Create test-24.js

---
+++ src/js/test-24.js
@@ -0,0 +1,9 @@
+function checkFruit(fruit) {
+  const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+  return fruits.includes(fruit) ? true : false; // Change this line
+}
+
+console.log(checkFruit('plum'));
+console.log(checkFruit('mandarin'));
+console.log(checkFruit('pear'));
