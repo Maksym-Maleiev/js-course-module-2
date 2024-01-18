f8fe9eb522af238869d3e13330b11937ac1b80bb
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-18 18:45:10 +0200

    Create test-23.js

---
+++ src/js/test-23.js
@@ -0,0 +1,19 @@
+function filterArray(numbers, value) {
+  // Change code below this line
+  let newArr = [];
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      newArr.push(numbers[i]);
+    }
+  }
+
+  return newArr;
+  // Change code above this line
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
