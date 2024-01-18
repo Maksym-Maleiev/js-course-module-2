12f53cd013faedcab7000fd75ef8341f5522df03
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-18 12:34:51 +0200

    Update test-20.js

---
+++ src/js/test-20.js
@@ -0,0 +1,14 @@
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+  for (let i = 0; i < order.length; i += 1) {
+    const element = order[i];
+    total += element;
+  }
+  // Change code above this line
+  return total;
+}
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
