2b5493f262655484fcfb8be1b87f25bb48fb4cbb
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-18 10:13:05 +0200

    Update test-05.js

---
+++ src/js/test-05.js
@@ -0,0 +1,10 @@
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// Change code below this line
+const firstElement = 'apple';
+const secondElement = 'plum';
+const lastElement = 'orange';
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[fruits.length - 1]);
