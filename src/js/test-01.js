5a6aab0a712855d75c75841643bcc4fb84d2bc2d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-18 08:25:06 +0200

    Create test-01.js

---
+++ src/js/test-01.js
@@ -0,0 +1,9 @@
+function checkAge(age) {
+  let message;
+  if (age >= 18) {
+    message = 'Вы совершеннолетний человек';
+  } else {
+    message = 'Вы не совершеннолетний человек';
+  }
+  return message;
+}
