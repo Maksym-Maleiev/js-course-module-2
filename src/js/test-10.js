9aa792b2531817dc78d8e45d9ae9e9d2adbda648
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-18 10:36:17 +0200

    Create test-10.js

---
+++ src/js/test-10.js
@@ -0,0 +1,13 @@
+function splitMessage(message, delimiter) {
+  let words;
+  // Change code below this line
+
+  // Change code above this line
+  return words;
+}
+
+console.log(splitMessage('Mango hurries to the train', ' '));
+
+console.log(splitMessage('Mango', ''));
+
+console.log(splitMessage('best_for_week', '_'));
