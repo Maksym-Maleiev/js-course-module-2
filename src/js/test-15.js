405ec257657e9e48e656363837dee75dc36bb191
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-18 11:10:37 +0200

    Create test-15.js

---
+++ src/js/test-15.js
@@ -0,0 +1,8 @@
+const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+const newClients = ['Peach', 'Houston'];
+
+const allClients = oldClients.concat(newClients); // Change this line
+
+console.log(oldClients);
+console.log(newClients);
+console.log(allClients);
