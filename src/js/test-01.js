a5b7409ab01e2bf3f873fedba018638f52086913
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-18 08:32:26 +0200

    Update test-01.js

--- src/js/test-01.js
+++ src/js/test-01.js
@@ -1,3 +1,4 @@
+/*
 function checkAge(age) {
   if (age >= 18) {
     return 'Ви повнолітня людина';
@@ -7,3 +8,18 @@ function checkAge(age) {
 }
 
 console.log(checkAge(18));
+*/
+
+function checkAge(age) {
+  if (age >= 18) {
+    // Change this line
+    return 'You are an adult';
+  }
+
+  return 'You are a minor';
+}
+
+console.log(checkAge(20));
+console.log(checkAge(8));
+console.log(checkAge(14));
+console.log(checkAge(38));
