68c0954cf8aa405e0cf1df1ded5e98e3bc75fc5d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-21 11:27:07 +0200

    Update test-32.js

--- src/js/test-32.js
+++ src/js/test-32.js
@@ -8,6 +8,8 @@ console.log(includes([1, 2, 3, 4, 5], 17));
 console.log(
   includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Jupiter')
 );
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes([1, 2, 3, 4, 5], 3));
+console.log(
+  includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus')
+);
+console.log(includes(['apple', 'plum', 'pear', 'orange'], 'plum'));
+console.log(includes(['apple', 'plum', 'pear', 'orange'], 'kiwi'));
