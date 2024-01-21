36ead9331244181e43f16594a17fb40eee66dd48
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-21 11:09:29 +0200

    Update test-29.js

--- src/js/test-29.js
+++ src/js/test-29.js
@@ -3,3 +3,9 @@ function getEvenNumbers(start, end) {
   for (let start = 0; i <= end; i += 1) {}
   // Change code above this line
 }
+
+console.log(getEvenNumbers(2, 5));
+console.log(getEvenNumbers(3, 11));
+console.log(getEvenNumbers(6, 12));
+console.log(getEvenNumbers(8, 8));
+console.log(getEvenNumbers(7, 7));
