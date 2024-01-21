d5d4ede9e29c02d0df4ed5dcbfa475e2958b1de7
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-21 11:16:55 +0200

    Update test-30.js

--- src/js/test-30.js
+++ src/js/test-30.js
@@ -1,9 +1,8 @@
-const start = 6;
-const end = 27;
-let number;
-
-for (let i = start; i < end; i += 1) {
-  if (i % 5 === 0) {
-    number = i;
+for (let i = 0; i <= 5; i += 1) {
+  console.log(i);
+  if (i === 3) {
+    console.log('Нашли число 3, прерываем выполнение цикла');
+    break;
   }
 }
+console.log('Лог после цикла');
