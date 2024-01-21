f0bcdaa8d6d1196008d6fe7dd1b5a4f1f87f51c8
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-21 11:17:03 +0200

    Update test-30.js

--- src/js/test-30.js
+++ src/js/test-30.js
@@ -1,8 +1,1 @@
-for (let i = 0; i <= 5; i += 1) {
-  console.log(i);
-  if (i === 3) {
-    console.log('Нашли число 3, прерываем выполнение цикла');
-    break;
-  }
-}
-console.log('Лог после цикла');
+
